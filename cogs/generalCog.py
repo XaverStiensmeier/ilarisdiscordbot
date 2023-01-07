1e84801ff44c1b4e2d40a6ae38564af0019ada33
XaverStiensmeier <xaverstiensmeier@gmx.de>
2023-01-07 12:28:21 +0100

    Improved comment.

--- cogs/generalCog.py
+++ cogs/generalCog.py
@@ -20,7 +20,7 @@ class GeneralCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(help="Greets the Ilaris Bot.")
     async def helloilaris(self, ctx):
         await ctx.send('Hello User!')
 
@@ -49,11 +49,9 @@ class GeneralCommands(commands.Cog):
     @commands.command(help="Rolls dice.", aliases=['w'])
     async def r(self, ctx,
                 roll: str = commands.parameter(default="III",
-                                               description="Dice string to parse. e.g.\n"
-                                                           "2d6+3: Sum the result of 2 "
-                                                           "6-sided dice and 3.\n"
-                                                           "2@3d20: Roll 3d20 and take the median i.e. "
-                                                           "(20,15,5) => 15.\n"
+                                               description="Dice string to parse. e.g. 2d6+3: Sum the result of 2 "
+                                                           "6-sided dice and 3. 2@3d20: Roll 3d20 and take the second "
+                                                           "highest i.e. (20,15,5) => 15.\n"
                                                            "Special rolls: I: 1d20, III: 2@3d20. Use 'I+' or 'III+' "
                                                            "to indicate fate point usage. "
                                                            "Use '++' with 'I++' or 'III++' to indicate fate point "
@@ -71,3 +69,29 @@ class GeneralCommands(commands.Cog):
     async def what(self, ctx):
         await ctx.author.send(file=discord.File(os.path.join(basic_paths.ROOT, "discord.log")))
         await ctx.author.send(file=discord.File(basic_paths.rjoin("groups/groups.yml")))
+
+    async def hilfe(self, ctx):
+        result = """
+        ```
+        **Generelle Befehle:**
+          card Nimmt einen Namen entgegen. Sendet ein Bild der angegebenen Regelkarte.
+          halloilaris Begrüßt den Ilaris Bot
+          ilaris Nimmt eine Zahl oder einen Namen entgegen. Postet die entsprechende Seite im Regelwerk oder Regel.
+          r Würfelt. 
+          what Nur für Administratoren: Ruft Debug-Informationen ab
+        **Gruppen Befehle:**
+          gcreate Erzeugt eine neue Gruppe mit dir als GM.
+          gdestroy Zerstört eine Gruppe, die du erstellt hast.
+          gjoin Tritt einer Gruppe als maximum_players bei
+          gleave Verlasse eine Gruppe als maximum_players
+          glist Listet alle beitretbaren Gruppen auf.
+          gremove Entfernt einen maximum_players aus deiner Gruppe
+          gset Setzt einen Gruppenschlüssel.
+          gsetdate Setzt das Datum.
+          gsetdescription Setzt die Beschreibung.
+          gsetnumberofplayers Setzt die Anzahl der maximum_players.
+
+        Gib !help command ein, um weitere Informationen zu einem Befehl auf Englisch zu erhalten
+        ```
+        """
+        await ctx.send(result)
