71d559e24ae1616a2dfb825ffd007ee99edd4f89
Lukas R <mail@sarbot.de>
2024-05-29 17:29:35 +0200

    move messages to ressources

--- messages.py
+++ messages.py
@@ -1,9 +1,10 @@
 import yaml
 import config as cfg
 
-with open('messages.yml', 'r', encoding="utf-8") as f:
+with open(cfg.RESOURCES/'messages.yml', 'r', encoding="utf-8") as f:
     msg = yaml.safe_load(f)
 
 if cfg.LANGUAGE != 'en':
-    with open(f'messages_{cfg.LANGUAGE}.yml', 'r', encoding="utf-8") as f:
+    lang_file = cfg.RESOURCES/f'messages_{cfg.LANGUAGE}.yml'
+    with open(lang_file, 'r', encoding="utf-8") as f:
         msg.update(yaml.safe_load(f))
